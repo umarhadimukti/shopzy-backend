898e5fd08dc44c049110a1eabe09174f138dad60
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-04 19:41:30 +0700

    added winston logger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT') || 3001);
+  const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
+  app.useLogger(logger);
+
+  const configService = app.get<ConfigService>(ConfigService);
+  await app.listen(configService.get<number>('PORT') || 3001);
 }
 bootstrap();
