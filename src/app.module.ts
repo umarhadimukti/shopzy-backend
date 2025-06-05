55870afed7b81295f211d5e53b0eb4d14c0e7b02
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-05 10:24:37 +0700

    added configuration for pino-logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const isProduction: boolean = configService.get('NODE_ENV') === 'production';
+        return {
+          pinoHttp: {
+            transport: isProduction ? undefined : { target: 'pino-pretty', options: { singleLine: true } },
+            level: isProduction ? 'info' : 'debug',
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
     UserModule,
   ],
   controllers: [],
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { Logger as PinoLogger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = app.get(PinoLogger);
+  app.useLogger(logger);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
