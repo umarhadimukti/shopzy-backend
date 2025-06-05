b3f6dcd03902a8cc211cff9688d4ffe821e8d4e3
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-05 10:10:17 +0700

    added class validator and transformer

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const configService = app.get<ConfigService>(ConfigService);
   await app.listen(configService.get<number>('PORT') || 3001);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 @Injectable()
 export class UserService {
+    constructor(
+        @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+    ) {}
+
     public async create(request: CreateUserRequest) {
+        this.logger.warn('ini testing');
         return {
-            name: 'test',
-            email: 'test@gmail.com',
-            password: 'test1234',
+            name: request.name,
+            email: request.email,
+            password: request.password,
         };
     }
 }
