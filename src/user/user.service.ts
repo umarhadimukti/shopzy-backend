669da51cf505303163a28b40106eeff9ee3c7626
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-05 17:06:13 +0700

    added create user service api

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,37 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
+import { Logger } from 'nestjs-pino';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
 @Injectable()
 export class UserService {
+    constructor(
+        private readonly logger: Logger,
+        private readonly prismaService: PrismaService,
+    ) {}
 
-    public async create(request: CreateUserRequest) {
-        return {
-            name: request.name,
-            email: request.email,
-            password: request.password,
-        };
+    public async create(request: CreateUserRequest): Promise<{ name: string, email: string }> {
+        this.logger.log(`creating user ${request.name}..`);
+
+        try {
+            return await this.prismaService.user.create({
+                data: {
+                    ...request,
+                    password: await bcrypt.hash(request.password, 10),
+                },
+                select: {
+                    name: true,
+                    email: true,
+                },
+            });
+        } catch (error) {
+            console.error(error)
+            if (error.code === 'P2002') {
+                throw new UnprocessableEntityException('email already exists');
+            }
+            throw error;
+        }
     }
 }
