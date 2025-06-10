50ce1363483001050a8eac7ad165d34a64e6b382
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-10 10:36:19 +0700

    registering cookie parser

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger as PinoLogger } from 'nestjs-pino';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
   app.useLogger(logger);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.use(cookieParser());
 
   const configService = app.get<ConfigService>(ConfigService);
   await app.listen(configService.get<number>('PORT') || 3001);
