4ed46eb54f5b3441a17f7554bf3712238fb0bfcc
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-10 14:15:09 +0700

    added jwt auth guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, UseInterceptors } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserRequest } from './dto/create-user.request';
 import { NoFilesInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/current-user.decorator';
+import { TokenPayload } from 'src/auth/auth.interface';
 
 @Controller('/api/users')
 export class UserController {
@@ -11,5 +14,13 @@ export class UserController {
   @UseInterceptors(NoFilesInterceptor())
   public async create(@Body() request: CreateUserRequest) {
     return this.userService.create(request);
+  }
+
+  @Get('/me')
+  @UseGuards(JwtAuthGuard)
+  public async getMe(
+    @CurrentUser() user: TokenPayload,
+  ) {
+    return user;
   }
 }
