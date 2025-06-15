4562249662465db21fbbe7f30ed7df639950b813
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-15 12:12:35 +0700

    added products schema

---
+++ prisma/migrations/20250615051146_create_products_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
