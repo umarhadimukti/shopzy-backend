04dfa5750f09d414e4a9c2099af7aaf424a44fa4
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-06-21 23:06:43 +0700

    added product upload interceptor

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, UseInterceptors } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateProductRequest } from './dto/create-product.request';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import { TokenPayload } from 'src/auth/auth.interface';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('/api/products')
 export class ProductController {
@@ -26,5 +29,21 @@ export class ProductController {
         @CurrentUser() user: TokenPayload,
     ) {
         return this.productService.getProducts(user.userId);
+    }
+
+    @Post('/:productId/image')
+    @UseGuards(JwtAuthGuard)
+    @UseInterceptors(
+        FileInterceptor('image', {
+            storage: diskStorage({
+                destination: 'public/products',
+                filename: (req, file, callback) => {
+                    callback(null, `${req.params.productId}${extname(file.originalname)}`);
+                },
+            }),
+        })
+    )
+    public async uploadProductImage() {
+
     }
 }
