91d056a757d66eb6e827209fc21d9c4316e78dbf
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-07-08 17:00:06 +0700

    import and export ws module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -25,5 +25,6 @@ import { JwtStrategy } from './strategies/jwt.strategy';
   ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { ProductGateway } from './product.gateway';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ProductController],
   providers: [ProductService, ProductGateway],
   exports: [ProductService],
