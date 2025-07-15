5ca44111eb5fb7422ca74f922c28499ff865121e
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-07-15 22:43:08 +0700

    set static asset cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger as PinoLogger } from 'nestjs-pino';
 import cookieParser from 'cookie-parser';
 import { DecimalInterceptor } from './common/decimal/decimal.interceptor';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const logger = app.get(PinoLogger);
   app.useLogger(logger);
@@ -17,6 +18,17 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.useGlobalInterceptors(new DecimalInterceptor());
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD'],
+  });
+
+  app.useStaticAssets('public', {
+    setHeaders: (res, path) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+    }
+  });
 
   const configService = app.get<ConfigService>(ConfigService);
   await app.listen(configService.get<number>('PORT') || 3001);
